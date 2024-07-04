c034745301abd7525729cab548b6df1d5c7857cb
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-07-04 16:15:16 +0100

    Rag Completed

--- src/main/java/com/example/demo/Config/RagConfig.java
+++ src/main/java/com/example/demo/Config/RagConfig.java
@@ -2,11 +2,66 @@ package com.example.demo.Config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.embedding.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Configuration
 public class RagConfig {
 
-   private static final Logger logger = LoggerFactory.getLogger(RagConfig.class);
+   private static final Logger log = LoggerFactory.getLogger(RagConfig.class);
+
+   @Value("classpath:/Docs/MarketAi.txt")
+    private Resource marketAI;
+
+   @Value("vectorstore.json")
+    private String vectorStore;
+
+   @Bean
+    SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingClient) {
+
+            SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingClient);
+            File marketAIFile = getMarketAIFile();
+            if(marketAIFile.exists()) {
+                log.info("Market AI file is found");
+
+                simpleVectorStore.load(marketAIFile);
+            } else {
+                log.info("Market AI file not found, creating new one");
+                TextReader textReader = new TextReader(marketAI);
+                textReader.getCustomMetadata().put("filename", "MarketAi.txt");
+                List<Document> documents = textReader.get();
+                TokenTextSplitter splitter = new TokenTextSplitter();
+                List<Document> splitDocs = splitter.apply(documents);
+
+                simpleVectorStore.add(splitDocs);
+                simpleVectorStore.save(marketAIFile);
+
+                simpleVectorStore.load(marketAI);
+
+            }
+            return simpleVectorStore;
+
+    }
+
+    private File getMarketAIFile() {
+
+       Path path = Paths.get("src" , "main" , "resources" , "Docs" );
+       String absolutePath = path.toFile().getAbsolutePath() + "/" +vectorStore;
+       return new File(absolutePath);
+
+    }
 
 }
--- src/main/java/com/example/demo/Controllers/ChatController.java
+++ src/main/java/com/example/demo/Controllers/ChatController.java
@@ -5,10 +5,17 @@ import com.example.demo.Service.OpenAiService;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.parser.BeanOutputParser;
 import org.springframework.ai.parser.ListOutputParser;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.core.io.Resource;
 import org.springframework.util.MimeTypeUtils;
 
 import groovy.util.logging.Slf4j;
@@ -29,6 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -41,10 +49,15 @@ public class ChatController {
     @Autowired
     private OpenAiService openAiService;
 
+    private final VectorStore vectorStore;
     private final ChatModel chatModel;
     private final ChatClient chatClient;
 
-    public ChatController( ChatModel chatModel , ChatClient.Builder chatClient) {
+    @Value("classpath:/Docs/RagPrompt.st")
+    private Resource ragPromtTemplate;
+
+    public ChatController(VectorStore vectorStore, ChatModel chatModel , ChatClient.Builder chatClient) {
+        this.vectorStore = vectorStore;
         this.chatModel = chatModel;
         this.chatClient = chatClient.build();
 
@@ -53,6 +66,20 @@ public class ChatController {
     @GetMapping("/chat")
     public Map<String, String> chat(@RequestParam(value = "prompt", defaultValue = "tell me a joke ") String prompt) {
         return Map.of("generation", chatModel.call(prompt));
+    }
+
+    @GetMapping("/Assistantchat")
+    public String assistantchat(@RequestParam(value = "prompt", defaultValue = "tell me about this app and why should i use it ") String prompt) {
+        List<Document>  similarDocs = vectorStore.similaritySearch(SearchRequest.query(prompt).withTopK(1));
+        List<String> contentList = similarDocs.stream().map(Document::getContent).toList();
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromtTemplate);
+        Map<String,Object> promptParams =new HashMap<>();
+        promptParams.put("input", prompt);
+        promptParams.put("documents", String.join("\n", contentList));
+        Prompt Ragedprompt = promptTemplate.create(promptParams);
+
+
+        return chatClient.call(Ragedprompt).getResult().getOutput().getContent();
     }
 
     @GetMapping("/Image")
