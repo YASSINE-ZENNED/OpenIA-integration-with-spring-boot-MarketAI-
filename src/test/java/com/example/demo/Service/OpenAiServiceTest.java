7159e7a3314b32c83cc515060a1f106a84f6e1a8
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-07-07 23:31:09 +0100

    started working on Unit tests

---
+++ src/test/java/com/example/demo/Service/OpenAiServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenAiServiceTest {
+
+    @Test
+    void generateImage() {
+    }
+
+    @Test
+    void supportChatWithImage() {
+    }
+
+    @Test
+    void describeImageAsUser() {
+    }
+
+    @Test
+    void describeImageAsEnterprise() {
+    }
+}
