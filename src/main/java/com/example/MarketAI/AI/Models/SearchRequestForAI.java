dc81e82f5b1cb90954f3dd9c4afde6873f7d39ab
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-09-13 14:12:49 +0100

    fixing some bugs

--- src/main/java/com/example/MarketAI/AI/Models/SearchRequestForAI.java
+++ src/main/java/com/example/MarketAI/AI/Models/SearchRequestForAI.java
@@ -1,24 +1,27 @@
 package com.example.MarketAI.AI.Models;
 
-import lombok.Data;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 
 
-@Data
 public class SearchRequestForAI {
 
-
-    //    private static final String TAVILY_API_KEY = "TAVILY_API_KEY";
-//
 //    Dotenv dotenv = Dotenv.load();
 //
-//    private final String secretKey = dotenv.get(TAVILY_API_KEY);
-    private final String api_key;
+//    // Access the variables
+//    String secretKey = dotenv.get("TAVILY_API_KEY");
+
+
+    @Value("${spring.tavily.api-key}")
+    private String api_key;
+
+    @Setter
     private String query;
 
     // Constructor only takes query as argument
     public SearchRequestForAI(String query) {
         this.query = query;
-        this.api_key = "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";
+        this.api_key = api_key;
 
     }
 
